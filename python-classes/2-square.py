0450eca5cecbd3b7dc60ee84e7abbb8a4e4a9f52
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-30 15:49:13 +0200

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,10 +3,12 @@
  
 
  class Square:
- """Define a class square"""
+ """ Represents a square
+    Attributes:
+        __size (int): size of a side of the square"""
 
     def __init__(self,size=0):
-        """initialize size of the square class """
+        """initializes the square Args:size (int): size of a side of the square Returns:None"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         else:

166f6f89f5efe39e3b35aa19a80adb8a5dd433da
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-30 15:54:51 +0200

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,13 +3,8 @@
 
 
 class Square:
-    """Represents a class square
-    Attributes:"""
+    """Represents a class square Attributes:"""
     
     def __init__(self, size):
-        """Initializes a square
-        Arguments:
-            size (int): size of a side of the square
-        Returns: Nothing
-        """
+        """Initializes square Args size (int): size of a side of the square Returns: Nothing"""
         self.__size = size

39ed29f1a7614d4e3c23296e6288196ba065724a
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-30 12:32:12 +0200

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" access and update the private attributes"""
+def size(self, value):
+    """ showing attributes of square class """
+    if type(size) is not int:
+        raise TypeError ("size must be an integer")
+    else:
+        if size > 0:
+            raise ValueError ("size must be >= 0")
+        else:
+            self.__size = value
+
+def __init__(self, size=0):
+    """ initializing size of square"""
+    self.size = size
+
+def area(self):
+    """ return area of calculated square"""
+    return (self.__size) ** 2

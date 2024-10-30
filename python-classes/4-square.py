e6b5dd9212c2d87902efb2d9e94f0c9b02e6b13d
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-30 13:18:12 +0200

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 """ access and update the private attributes"""
-def size(self, value):
+class Square:
+   """ define class square attributes"""
+    def size(self, value):
+
     """ showing attributes of square class """
-    if type(size) is not int:
-        raise TypeError ("size must be an integer")
-    else:
-        if size > 0:
-            raise ValueError ("size must be >= 0")
+        if type(size) is not int:
+
+            raise TypeError ("size must be an integer")
+        else:
+
+            if size > 0:
+
+               raise ValueError ("size must be >= 0")
+
         else:
             self.__size = value
 
-def __init__(self, size=0):
-    """ initializing size of square"""
-    self.size = size
+    def __init__(self, size=0):
 
-def area(self):
+    """ initializing size of square"""
+        self.size = size
+
+    def area(self):
     """ return area of calculated square"""
-    return (self.__size) ** 2
+        return (self.__size) ** 2

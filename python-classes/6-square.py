5359766afce0f66c9800723ec53d0a25b21195bd
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-30 12:59:10 +0200

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,6 +10,7 @@ class Square:
     def area(self):
         """calculates the square's area Returns The area of the square"""
         return (self.__size) ** 2
+    @property
 
     def size(self):
         """ The size of the square"""

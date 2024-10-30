a4f60a9a5ec4e77181443b120d942ed1b1e1bb77
h-mutumwinka <h.mutumwink@alustudent.com>
2024-10-30 12:08:41 +0200

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+ class Square
+ """Define a class square"""
+    def __init__(self,size=0):
+        """initialize size of the square class """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size > 0
+            raise ValueError(size must be >= 0)
+        else:
+            self.__size = size
